d28c89164851e9ca903047698a6fafbc3d9f3a86
jagomez7411 <aredcbtis16@hotmail.com>
2018-06-25 09:01:21 -0500

    16

--- js/acciones.js
+++ js/acciones.js
@@ -27,13 +27,10 @@ $('#watch').on('click', function (){
 
    function onSuccess(position) {
 
-      alert('Latitude: '          + position.coords.latitude          + '\n' +
+      alert('Latitude: '       + position.coords.latitude          + '\n' +
          'Longitude: '         + position.coords.longitude         + '\n' +
          'Altitude: '          + position.coords.altitude          + '\n' +
          'Accuracy: '          + position.coords.accuracy          + '\n' +
-         'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-         'Heading: '           + position.coords.heading           + '\n' +
-         'Speed: '             + position.coords.speed             + '\n' +
          'Timestamp: '         + position.timestamp                + '\n');
    };
 
